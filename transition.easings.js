8738bbb83c70894c382b0689566c2decff4d252c
Paciente8159 <Paciente8159@users.noreply.github.com>
2020-12-19 13:29:41 +0000

    initial release

---
+++ transition.easings.js
@@ -0,0 +1,158 @@
+"use strict";
+
+transitionJS.prototype.easeInQuad = function (x) {
+  return x * x;
+};
+
+transitionJS.prototype.easeOutQuad = function (x) {
+  return 1 - (1 - x) * (1 - x);
+};
+
+transitionJS.prototype.easeInOutQuad = function (x) {
+  return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+};
+
+transitionJS.prototype.easeInCubic = function (x) {
+  return x * x * x;
+};
+
+transitionJS.prototype.easeOutCubic = function (x) {
+  return 1 - Math.pow(1 - x, 3);
+};
+
+transitionJS.prototype.easeInOutCubic = function (x) {
+  return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+};
+
+transitionJS.prototype.easeInQuart = function (x) {
+  return x * x * x * x;
+};
+
+transitionJS.prototype.easeOutQuart = function (x) {
+  return 1 - Math.pow(1 - x, 4);
+};
+
+transitionJS.prototype.easeInOutQuart = function (x) {
+  return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+};
+
+transitionJS.prototype.easeInQuint = function (x) {
+  return x * x * x * x * x;
+};
+
+transitionJS.prototype.easeOutQuint = function (x) {
+  return 1 - Math.pow(1 - x, 5);
+};
+
+transitionJS.prototype.easeInOutQuint = function (x) {
+  return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+};
+
+transitionJS.prototype.easeInSine = function (x) {
+  return 1 - Math.cos((x * PI) / 2);
+};
+
+transitionJS.prototype.easeOutSine = function (x) {
+  return Math.sin((x * PI) / 2);
+};
+
+transitionJS.prototype.easeInOutSine = function (x) {
+  return -(Math.cos(PI * x) - 1) / 2;
+};
+
+transitionJS.prototype.easeInExpo = function (x) {
+  return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
+};
+
+transitionJS.prototype.easeOutExpo = function (x) {
+  return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+};
+
+transitionJS.prototype.easeInOutExpo = function (x) {
+  return x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : x < 0.5
+    ? Math.pow(2, 20 * x - 10) / 2
+    : (2 - Math.pow(2, -20 * x + 10)) / 2;
+};
+
+transitionJS.prototype.easeInCirc = function (x) {
+  return 1 - Math.sqrt(1 - Math.pow(x, 2));
+};
+
+transitionJS.prototype.easeOutCirc = function (x) {
+  return Math.sqrt(1 - Math.pow(x - 1, 2));
+};
+
+transitionJS.prototype.easeInOutCirc = function (x) {
+  return x < 0.5
+    ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
+    : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+};
+
+transitionJS.prototype.easeInBack = function (x) {
+  return c3 * x * x * x - c1 * x * x;
+};
+
+transitionJS.prototype.easeOutBack = function (x) {
+  return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+};
+
+transitionJS.prototype.easeInOutBack = function (x) {
+  return x < 0.5
+    ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+    : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+};
+
+transitionJS.prototype.easeInElastic = function (x) {
+  return x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
+};
+
+transitionJS.prototype.easeOutElastic = function (x) {
+  return x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+};
+
+transitionJS.prototype.easeInOutElastic = function (x) {
+  return x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : x < 0.5
+    ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+    : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+};
+
+transitionJS.prototype.easeOutBounce = function (x) {
+  const n1 = 7.5625;
+  const d1 = 2.75;
+
+  if (x < 1 / d1) {
+    return n1 * x * x;
+  } else if (x < 2 / d1) {
+    return n1 * (x -= 1.5 / d1) * x + 0.75;
+  } else if (x < 2.5 / d1) {
+    return n1 * (x -= 2.25 / d1) * x + 0.9375;
+  } else {
+    return n1 * (x -= 2.625 / d1) * x + 0.984375;
+  }
+};
+
+transitionJS.prototype.easeInBounce = function (x) {
+    return 1 - this.easeOutBounce(1 - x);
+  };
+
+transitionJS.prototype.easeInOutBounce = function (x) {
+  return x < 0.5
+    ? (1 - this.easeOutBounce(1 - 2 * x)) / 2
+    : (1 + this.easeOutBounce(2 * x - 1)) / 2;
+};
---
+++ transition.js
@@ -0,0 +1,171 @@
+"use strict";
+
+function transitionJS(options) {
+  const requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+  const cancelAnimationFrame =
+    window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+
+  this.options = this.defaultOptions(options);
+  //max to 110% the frame rate
+  this.frameTime = 900 / this.options.fps;
+  this.startTime = 0;
+  this.lastTime = 0;
+  this.transitionID = 0;
+  this.rendering = false;
+  this.easing = this.selectEasing(this.options.transitionEasing);
+}
+
+transitionJS.prototype.now = function(){
+  if(performance.now){
+    return performance.now();
+  }
+
+  if (Date.now) {
+    return Date.Now();
+  }
+  
+  return new Date().getTime();
+}
+
+transitionJS.prototype.startTransition = function () {
+  this.lastTime = 0;
+  this.startTime = this.now();
+  this.lastTime = this.startTime;
+  this.rendering = true;
+  this.run();
+};
+
+transitionJS.prototype.cancelTransition = function () {
+  cancelAnimationFrame(this.transitionID);
+  this.rendering = false;
+  this.lastTime = 0;
+  this.startTime = 0;
+};
+
+transitionJS.prototype.run = function () {
+  this.transitionID = requestAnimationFrame(this.run.bind(this));
+  var current = this.now();
+  var elapsed = (current - this.lastTime);
+  //nothing to render leave
+  if(elapsed < this.frameTime){
+    return;
+  }
+
+  var elapsed_total = Math.min(this.options.transitionDuration, (current - this.startTime));
+  var percentage = elapsed_total / this.options.transitionDuration;
+  var framepercentage = (!this.easing) ? percentage : this.easing(percentage);
+
+  if (this.options.renderFrame) {
+    this.options.renderFrame(percentage, framepercentage);
+  }
+
+  if (percentage == 1 && !this.rendering) {
+    cancelAnimationFrame(this.transitionID);
+    if (this.options.transitionEnded) {
+      this.options.transitionEnded();
+    }
+  }
+
+  this.lastTime = current;
+  this.rendering = (percentage != 1);
+};
+
+transitionJS.prototype.selectEasing = function (easing) {
+  if (typeof easing === "string") {
+    switch (easing) {
+      case "easeInQuad":
+        return this.easeInQuad;
+      case "easeOutQuad":
+        return this.easeOutQuad;
+      case "easeInOutQuad":
+        return this.easeInOutQuad;
+      case "easeInCubic":
+        return this.easeInCubic;
+      case "easeOutCubic":
+        return this.easeOutCubic;
+      case "easeInOutCubic":
+        return this.easeInOutCubic;
+      case "easeInQuart":
+        return this.easeInQuart;
+      case "easeOutQuart":
+        return this.easeOutQuart;
+      case "easeInOutQuart":
+        return this.easeInOutQuart;
+      case "easeInQuint":
+        return this.easeInQuint;
+      case "easeOutQuint":
+        return this.easeOutQuint;
+      case "easeInOutQuint":
+        return this.easeInOutQuint;
+      case "easeInSine":
+        return this.easeInSine;
+      case "easeOutSine":
+        return this.easeOutSine;
+      case "easeInOutSine":
+        return this.easeInOutSine;
+      case "easeInExpo":
+        return this.easeInExpo;
+      case "easeOutExpo":
+        return this.easeOutExpo;
+      case "easeInOutExpo":
+        return this.easeInOutExpo;
+      case "easeInCirc":
+        return this.easeInCirc;
+      case "easeOutCirc":
+        return this.easeOutCirc;
+      case "easeInOutCirc":
+        return this.easeInOutCirc;
+      case "easeInBack":
+        return this.easeInBack;
+      case "easeOutBack":
+        return this.easeOutBack;
+      case "easeInOutBack":
+        return this.easeInOutBack;
+      case "easeInElastic":
+        return this.easeInElastic;
+      case "easeOutElastic":
+        return this.easeOutElastic;
+      case "easeInOutElastic":
+        return this.easeInOutElastic;
+      case "easeOutBounce":
+        return this.easeOutBounce;
+      case "easeInBounce":
+        return this.easeInBounce;
+      case "easeInOutBounce":
+        return this.easeInOutBounce;
+    }
+  }
+
+  return easing;
+};
+
+transitionJS.prototype.defaultOptions = function (options) {
+  var fulloptions = {
+    /**
+     * FPS options
+     */
+    fps: 60,
+    transitionDuration: 1000,
+
+    /**
+     * Callback functions
+     */
+    renderFrame: null,
+    transitionEasing: null,
+    transitionEnded: null,
+  };
+
+  Object.getOwnPropertyNames(options).forEach(function (element) {
+    Object.defineProperty(fulloptions, element, {
+      value: options[element],
+      writable: true,
+    });
+  });
+
+  return fulloptions;
+};
