3acf42e823e927269d218a728076b8874f6bdce8
Paciente8159 <Paciente8159@users.noreply.github.com>
2021-02-13 23:01:09 +0000

    fixed eases and updated minified files
    -fixed easing functions easeXXXBack missing constants
    -updated minified fiels

--- transition.easings.js
+++ transition.easings.js
@@ -93,14 +93,20 @@ transitionJS.prototype.easeInOutCirc = function (x) {
 };
 
 transitionJS.prototype.easeInBack = function (x) {
+  const c1 = 1.70158;
+  const c3 = c1 + 1;
   return c3 * x * x * x - c1 * x * x;
 };
 
 transitionJS.prototype.easeOutBack = function (x) {
+  const c1 = 1.70158;
+  const c3 = c1 + 1;
   return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
 };
 
 transitionJS.prototype.easeInOutBack = function (x) {
+  const c1 = 1.70158;
+  const c2 = c1 * 1.525;
   return x < 0.5
     ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
     : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
